c3d28e1b341f9ec625da0842069309d51b1859a4
Jacob Wylie <jacobwylie@gmail.com>
2017-05-23 20:32:34 +0200

    Add files via upload

---
+++ gulpfile.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const gulp = require('gulp'),
+	  concat = require('gulp-concat'),
+	  minCss = require('gulp-minify-css'),
+	  rename = require('gulp-rename'),
+	  uglify = require('gulp-uglify'),
+	  stripDebug = require('gulp-strip-debug'),
+	  annotate = require('gulp-ng-annotate');
+
+gulp.task('doIt', () => {
+    //Concat and Minify CSS
+    gulp.src(['./css/bootstrap-reboot.min.css',
+    		  './css/bootstrap-grid.min.css',
+    		  './css/bootstrap.min.css',
+    		  './css/slick.css',
+    		  './css/slick-theme.css',
+    		  './css/vegas.min.css',
+    		  './css/style.css',])
+        .pipe(concat('build.css'))
+        //.pipe(sourcemaps.init())
+        .pipe(minCss({
+            keepSpecialComments: 0
+        }))
+        //.pipe(sourcemaps.write('.'))
+        .pipe(rename('build.min.css'))
+        .pipe(gulp.dest('./css'));
+
+    //Combine JS Files into core.js
+    gulp.src(['./js/jquery-3.2.1.min.js',
+    		   './js/tether.min.js',
+    		   './js/bootstrap.js',
+    		   './js/vegas.min.js',
+    		   './js/slick.min.js',
+    		   './js/script.js'])
+        .pipe(concat('bundle.js'))
+        .pipe(stripDebug())
+        .pipe(annotate()) // Makes angular safe to minify.
+        .pipe(uglify()) // Minifies the concatenated js file.
+        .pipe(rename('bundle.min.js'))
+        .pipe(gulp.dest('./js'));
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
